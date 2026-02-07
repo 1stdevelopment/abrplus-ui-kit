4a4e05cd922affd7d15ed73b501ef0744ed238bd
Shervin Ghajar <ssghajar.work@gmail.com>
2026-02-07 10:34:47 +0330

    chore: config semantic rc

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
   branches: ['develop'],
-
-  tagFormat: false,
-
+  tagFormat: 'v${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
         npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],

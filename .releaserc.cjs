988a0ac0ea8b3ced54e6a8bb8a59531309c30957
Shervin Ghajar <ssghajar.work@gmail.com>
2026-02-07 20:13:32 +0330

    fix: semanticrc

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -9,12 +9,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    // [
-    //   '@semantic-release/npm',
-    //   {
-    //     npmPublish: true,
-    //   },
-    // ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     [
       '@semantic-release/git',
       {
